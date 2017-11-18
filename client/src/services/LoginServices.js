e1b5fb3303cf205275eff92e42a80d170066778d
CarlosSanches07 <carlos-caps@holtmail.com>
2017-11-18 21:23:14 -0200

    fixed some bugs

--- client/src/services/LoginServices.js
+++ client/src/services/LoginServices.js
@@ -3,9 +3,9 @@ import api from '@/services/api'
 export default {
   doLogin (params) {
     api.post('/user/login', params)
-  }
+  },
 
   doSignIn (params) {
-  	api.post('/user/post', params)
+    api.post('/user/post', params)
   }
 }
