1861e69c403ee501c3eedc22c49c13dd6f0ace5b
NetoBoralli <netoboralli@gmail.com>
2017-11-22 18:38:31 -0200

    commit

--- server/src/models/Model.Project.js
+++ server/src/models/Model.Project.js
@@ -8,7 +8,7 @@ export default class ModelProject {
 	getById(callback) {
 		let conn = new database();
 		conn.connection((data) => {
-			data.collection('projects').find({ "managers": { "user_mng_name": this._data.name } }).toArray()
+			data.collection('projects').find({ "managers": this._data.name }).toArray()
 				.then((data) => {
 
 					callback(data);
